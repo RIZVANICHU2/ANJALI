832faa4e6ef54abb97348b0dc917e5f7051e6dd9
muhammed-usrbot <93420716+muhammed-usrbot@users.noreply.github.com>
2021-12-31 10:55:01 +0530

    ꪶɪͥᴛͭsᷤ ͢ᴍͫᴇͤᡃᴛᴀᴜʀᴜs࿐

---
+++ Taurus/workon.js
@@ -0,0 +1,14 @@
+/* Codded by *@muhammed-usrbot
+
+Telegram: t.me/taurus_sir
+Instagram: https://www.instagram.com/taurus.efx
+
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+
+Taurus x3 - Muhammed
+*/
+
+*\RUN git clone :www.github.com/muhammed-usrbot/baileys
+all in one git :get clone ../Taurus.py/
+Copyright © 2021 @license.muhammed-usrbot.github.io
