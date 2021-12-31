76dc0cfd2ec986a99d0b0b2cf5d4266b99260762
IAM-TOXIDE-SER <96900308+IAM-TOXIDE-SER@users.noreply.github.com>
2021-12-31 23:06:43 +0530

    𝙷𝙴𝙿𝙿𝚈 𝙽𝙴𝚆 𝙴𝙰𝚁

--- message/mess.js
+++ message/mess.js
@@ -1,9 +1,9 @@
 module.exports.mess = {
 	        wait: '*𝚆𝙰𝙸𝚃 𝙲𝙷𝙴𝚈𝚈 𝙼𝚆𝙾𝙽𝚄 𝚂𝙷𝙴𝙼𝙰 𝚅𝙴𝙽𝙰𝙼👻*',
 			success: '*𝚄𝙳𝙷𝙴𝚂𝙷𝙸𝙲𝙷𝙰 𝚂𝙰𝙼𝙱𝙰𝚅𝙰𝙼 𝙽𝙰𝙳𝙰𝙽𝙽𝚄👻*',
-			wrongFormat: '*Format Is Wrong Retry..!*',
+			wrongFormat: '*𝙵𝙾𝚁𝙼𝙰𝚃 𝚃𝙷𝙴𝚃𝚃 𝙰𝙰𝙽 𝙼𝚆𝙾𝙽𝚄*',
 			error: {
-				api: '*Oops An Error Occurred*',
+				api: '*𝚃𝙷𝙰𝚃𝙷 𝙲𝙾𝙼𝙼𝙰𝙽𝙳 𝙸𝚂 𝙽𝙾𝚃 𝙸𝙽 𝙾𝚄𝚁 𝙼𝙴𝙽𝚄 𝚃𝚈𝙿𝙴.𝙼𝙴𝙽𝚄*',
 				stick: '*Not That Sticker :v*',
 				Iv: '*Invalid Link*'
 			},
