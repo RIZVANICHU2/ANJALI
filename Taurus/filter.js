a78995e6b38e0fd020af5f4777f3746d8792e035
muhammed-usrbot <93420716+muhammed-usrbot@users.noreply.github.com>
2021-12-31 10:51:23 +0530

    ꪶɪͥᴛͭsᷤ ͢ᴍͫᴇͤᡃᴛᴀᴜʀᴜs࿐

--- Taurus/filter.js
+++ Taurus/filter.js
@@ -1,1 +1,23 @@
-['Taurus','Muhammed','Bot','help','menu','@919961050829','@918157849715']
+/* Codded by *@muhammed-usrbot
+
+Telegram: t.me/taurus_sir
+Instagram: https://www.instagram.com/taurus.efx
+
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+
+Taurus x3 - Muhammed
+*/
+bgm[
+       'Taurus
+       'Muhammed
+       'Bot
+       'help
+       'menu
+       '@919961050829
+       '@918157849715'
+            ]
+stick[
+       'git
+       'hii
+          ]
