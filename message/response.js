dce69937d7f0a98b08063b88e616f5ecceef236a
IAM-TOXIDE-SER <96900308+IAM-TOXIDE-SER@users.noreply.github.com>
2022-01-02 11:49:15 +0530

    Create response.js

---
+++ message/response.js
@@ -0,0 +1,31 @@
+{
+
+        if (budy.includes("Toxide")){  // respon tag ubah aja
+
+        reply(`*${pushname2}*, 𝚃𝙾𝚇𝙸𝙳𝙴 𝚂𝙴𝚁 𝙾𝙵𝙵𝙻𝙸𝙽𝙴 𝙴𝙽𝙽 𝚃𝙷𝙾𝙽𝚄𝙽𝙽𝚄 𝚃𝚈𝙿𝙴 !𝙲𝙾𝙾𝚆𝙽𝙴𝚁 𝙽𝚄𝙼𝙱𝙴𝚁 𝙺𝙸𝚃𝚃𝚄𝙼?`)
+
+        const d = fs.readFileSync('./sticker/anjim.webp');
+
+        denz.sendMessage(from, d, sticker, {quoted: mek, quoted: { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(from ? { remoteJid: "status@broadcast" } : {}) }, message: { "imageMessage": { "url": "https://mmg.whatsapp.net/d/f/At0x7ZdIvuicfjlf9oWS6A3AR9XPh0P-hZIVPLsI70nM.enc", "mimetype": "image/jpeg","caption": "_𝙏𝙊𝙓𝙄𝘿𝙀 𝙎𝙀𝙍_", 'jpegThumbnail': fs.readFileSync('./sticker/dnsnew.webp')}}}})
+
+        }
+
+        if (budy.includes("Anjali")){  // respon tag ubah aja
+
+        reply(`*${pushname2}*, 𝙽𝙹𝙰𝙽 𝙺𝙰𝙺𝙾𝙾𝚂𝙻𝙰 𝙽𝚃𝙷𝙴𝙻𝙼 𝚅𝙴𝙽𝙰𝙼 𝙽𝙸𝙽𝙺?`)
+
+        const d = fs.readFileSync('./sticker/anjim.webp');
+
+        denz.sendMessage(from, d, sticker, {quoted: mek, quoted: { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(from ? { remoteJid: "status@broadcast" } : {}) }, message: { "imageMessage": { "url": "https://mmg.whatsapp.net/d/f/At0x7ZdIvuicfjlf9oWS6A3AR9XPh0P-hZIVPLsI70nM.enc", "mimetype": "image/jpeg","caption": "_𝙏𝙊𝙓𝙄𝘿𝙀 𝙎𝙀𝙍_", 'jpegThumbnail': fs.readFileSync('./sticker/dnsnew.webp')}}}})
+
+        }
+
+        if (budy.includes("Akshay")){  // respon tag ubah aja
+
+        reply(`*${pushname2}*, 𝚃𝙾𝚇𝙸𝙳𝙴 𝚂𝙴𝚁 𝙾𝙵𝙵𝙻𝙸𝙽𝙴 𝙴𝙽𝙽 𝚃𝙷𝙾𝙽𝚄𝙽𝙽𝚄 𝚃𝚈𝙿𝙴 !𝙲𝙾𝙾𝚆𝙽𝙴𝚁 𝙽𝚄𝙼𝙱𝙴𝚁 𝙺𝙸𝚃𝚃𝚄𝙼?`)
+
+        const d = fs.readFileSync('./sticker/anjim.webp');
+
+        denz.sendMessage(from, d, sticker, {quoted: mek, quoted: { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(from ? { remoteJid: "status@broadcast" } : {}) }, message: { "imageMessage": { "url": "https://mmg.whatsapp.net/d/f/At0x7ZdIvuicfjlf9oWS6A3AR9XPh0P-hZIVPLsI70nM.enc", "mimetype": "image/jpeg","caption": "_𝙏𝙊𝙓𝙄𝘿𝙀 𝙎𝙀𝙍_", 'jpegThumbnail': fs.readFileSync('./sticker/dnsnew.webp')}}}})
+
+        }
